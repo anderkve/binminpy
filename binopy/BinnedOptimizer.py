e4798eeed09fd396672537b65950300604448cc5
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2025-02-20 16:17:50 +0100

    Added first parallelized example, using concurrent.futures.ProcessPoolExecutor.

--- binopy/BinnedOptimizer.py
+++ binopy/BinnedOptimizer.py
@@ -13,6 +13,7 @@ class BinnedOptimizer:
         self.target_function = target_function
         self.binning_tuples = binning_tuples  # A list on the form [(x1_min, x1_max, n_bins_x1), (x2_min, x2_max, n_bins_x2), ...]
         self.optimizer_kwargs = optimizer_kwargs
+        self.max_processes = max_processes
 
         self.n_dims = len(binning_tuples)
         self.n_bins_per_dim = [bt[2] for bt in binning_tuples]
@@ -40,6 +41,44 @@ class BinnedOptimizer:
 
         return res
 
+
+
+
+    def run(self):
+        """Start the optimization"""
+
+
+        # Do the binning of the input space:
+        # 
+        # - Each bin needs
+        #   1) a unique index
+        #   2) a corresponding bounds list with the bin limits in each direction
+        # 
+        # - Save this in a list called "all_bounds", such that all_bounds[i]
+        #   contains the bounds corresponding to bin i
+
+        # Dummy example for 3 bins and a 1D function:
+        all_bounds = [
+            [(-2,2)], 
+            [(2,4)], 
+            [(4,6)],
+            [(6,8)],
+        ]
+
+        # Use ProcessPoolExecutor for parallel execution
+        collected_results = None
+        with ProcessPoolExecutor(max_workers=self.max_processes) as executor:
+
+            # Map the bins to the worker_function in parallel
+            collected_results = executor.map(self.worker_function, all_bounds)
+
+            # # Fill the results into the numpy array
+            # for idx, result in enumerate(results):
+            #     grid_results_array[idx, :] = result
+            #     if idx % 100 == 0:
+            #         print(f"- Done points: {idx}")
+
+        return collected_results
 
 
 # Bruk samme stratgi som i example_code__hyperpar_loglike_gridscan.py.
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import numpy as np
 from binopy.BinnedOptimizer import BinnedOptimizer
+from time import sleep
 
 
 # User target function
 def target_function(x):
     result = 10. + np.sum(x**2)
+    sleep(0.5)
+    print(f"x: {x}   target_function: {result}")
     return result
 
 
@@ -13,14 +16,18 @@ binning_tuples = [(-3,3,1), (-3,3,1)]
 
 optimizer_kwargs = {
     "method": "L-BFGS-B",
-    "tol": 1e-9,
+    "tol": 1e-10,
 }
 
-binned_opt = BinnedOptimizer(target_function, binning_tuples, optimizer_kwargs=optimizer_kwargs, max_processes=1)
+max_processes = 4
+binned_opt = BinnedOptimizer(target_function, binning_tuples, optimizer_kwargs=optimizer_kwargs, max_processes=max_processes)
+
+collected_results = binned_opt.run()
+
+for i,res in enumerate(collected_results):
+    print(f"------ Bin {i} ------ ")
+    print(res)
+    print()
 
 
-bounds = [(-3,3), (-3,3)]
-res = binned_opt.worker_function(bounds)
-
-print(res)
 
