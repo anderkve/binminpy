673387450c0e33d466cefb2c0aee63dc3133aa95
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2025-02-21 08:31:54 +0100

    Added functionality for returning all the evaluated points.

--- binopy/BinnedOptimizer.py
+++ binopy/BinnedOptimizer.py
@@ -1,30 +1,48 @@
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
-from scipy.optimize import minimize
+from scipy.optimize import minimize, OptimizeResult
 from copy import copy
 import warnings
 
 
 class BinnedOptimizer:
 
-    def __init__(self, target_function, binning_tuples, optimizer_kwargs={}, max_processes=1):
+    def __init__(self, target_function, binning_tuples, optimizer_kwargs={}, max_processes=1, return_evaluations=False):
         """Constructor"""
 
         self.target_function = target_function
         self.binning_tuples = binning_tuples  # A list on the form [(x1_min, x1_max, n_bins_x1), (x2_min, x2_max, n_bins_x2), ...]
         self.optimizer_kwargs = optimizer_kwargs
         self.max_processes = max_processes
+        self.return_evaluations = return_evaluations
 
         self.n_dims = len(binning_tuples)
         self.n_bins_per_dim = [bt[2] for bt in binning_tuples]
+
+        self.print_prefix = "BinnedOptimizer:"
 
         if "bounds" in self.optimizer_kwargs:
             warnings.warn("BinnedOptimizer will override the 'bounds' entry provided via the 'optimizer_kwargs' dictionary.")
             del(self.optimizer_kwargs["bounds"])
 
 
-    def worker_function(self, bounds):
+    def _worker_function(self, bounds):
         """Function to optimize the target function within a set of bounds"""
+
+        use_optimizer_kwargs = copy(self.optimizer_kwargs)
+
+        # Lists to store function evaluations
+        x_points = []
+        y_points = []
+
+        # Wrapper for the target function, to allow us to save the evaluations
+        def target_function_wrapper(x, *args):
+            y = self.target_function(x, *args)
+            print(f"{self.print_prefix} target_function_wrapper:  x: {x}  args: {args}  y: {y}")
+            if self.return_evaluations:
+                x_points.append(x)
+                y_points.append(y)
+            return y
 
         # Initial point for the optimization
         x0 = np.array([0.5 * (x_min + x_max) for x_min,x_max in bounds])
@@ -32,21 +50,21 @@ class BinnedOptimizer:
         # Do the optimization and store the result
         res = None
         try:
-            res = minimize(self.target_function, x0, bounds=bounds, **self.optimizer_kwargs)
+            res = minimize(target_function_wrapper, x0, bounds=bounds, **use_optimizer_kwargs)
         except ValueError as e:
-            warnings.warn(f"Optimization attempt returned ValueError ({e}). Trying again with method='trust-constr'.", RuntimeWarning)
-            modified_optimizer_kwargs = copy(self.optimizer_kwargs)
-            modified_optimizer_kwargs["method"] = "trust-constr"
-            res = minimize(self.target_function, x0, bounds=bounds, **modified_optimizer_kwargs)
+            warnings.warn(f"{self.print_prefix} Optimization attempt returned ValueError ({e}). Trying again with method='trust-constr'.", RuntimeWarning)
+            use_optimizer_kwargs["method"] = "trust-constr"
+            res = minimize(target_function_wrapper, x0, bounds=bounds, **use_optimizer_kwargs)
 
-        return res
-
+        if self.return_evaluations:
+            return res, np.array(x_points), np.array(y_points)
+        else:
+            return res
 
 
 
     def run(self):
         """Start the optimization"""
-
 
         # Do the binning of the input space:
         # 
@@ -57,29 +75,43 @@ class BinnedOptimizer:
         # - Save this in a list called "all_bounds", such that all_bounds[i]
         #   contains the bounds corresponding to bin i
 
-        # Dummy example for 3 bins and a 1D function:
+        # Dummy example for a 2D function:
         all_bounds = [
-            [(-2,2)], 
-            [(2,4)], 
-            [(4,6)],
-            [(6,8)],
+            [(-2,2), (-2,2)], 
+            [(-2,2), (2,4)], 
+            [(-2,2), (4,6)],
+            [(-2,2), (6,8)],
+
+            [(-2,2), (-2,2)], 
+            [(-2,2), (2,4)], 
+            [(-2,2), (4,6)],
+            [(-2,2), (6,8)],
+
+            [(-2,2), (-2,2)], 
+            [(-2,2), (2,4)], 
+            [(-2,2), (4,6)],
+            [(-2,2), (6,8)],
+
+            [(-2,2), (-2,2)], 
+            [(-2,2), (2,4)], 
+            [(-2,2), (4,6)],
+            [(-2,2), (6,8)],
         ]
 
+        n_bins = len(all_bounds)
+
+        collected_outputs = [None] * n_bins
+
         # Use ProcessPoolExecutor for parallel execution
-        collected_results = None
         with ProcessPoolExecutor(max_workers=self.max_processes) as executor:
 
-            # Map the bins to the worker_function in parallel
-            collected_results = executor.map(self.worker_function, all_bounds)
+            # Create a generator for all the tasks
+            task_mapping = executor.map(self._worker_function, all_bounds)
 
-            # # Fill the results into the numpy array
-            # for idx, result in enumerate(results):
-            #     grid_results_array[idx, :] = result
-            #     if idx % 100 == 0:
-            #         print(f"- Done points: {idx}")
+            # Now execute the tasks in parallel and collect the results 
+            for bin_id, result in enumerate(task_mapping):
+                collected_outputs[bin_id] = result
+                print(f"{self.print_prefix} Task {bin_id} is done.", flush=True)
 
-        return collected_results
+        return collected_outputs
 
-
-# Bruk samme stratgi som i example_code__hyperpar_loglike_gridscan.py.
-# Det burde også passe bra for MPI-parallelisering med master-worker...
--- test.py
+++ test.py
@@ -2,32 +2,56 @@ import numpy as np
 from binopy.BinnedOptimizer import BinnedOptimizer
 from time import sleep
 
-
+#
 # User target function
-def target_function(x):
-    result = 10. + np.sum(x**2)
-    sleep(0.5)
-    print(f"x: {x}   target_function: {result}")
+#
+
+def target_function(x, a):
+
+    result = a + np.sum(x**2) 
+
+    # sleep(0.5)
+    # Waste some time in way that keeps the CPU working
+    for i in range(int(1e7)):
+        pass
+
     return result
 
 
+#
 # Test setup
-binning_tuples = [(-3,3,1), (-3,3,1)]
+#
+
+binning_tuples = [(-3,3,1), (-3,3,1)]  # Currently not used
 
 optimizer_kwargs = {
     "method": "L-BFGS-B",
     "tol": 1e-10,
+    "args": (10),
 }
 
 max_processes = 4
-binned_opt = BinnedOptimizer(target_function, binning_tuples, optimizer_kwargs=optimizer_kwargs, max_processes=max_processes)
 
-collected_results = binned_opt.run()
+binned_opt = BinnedOptimizer(
+    target_function, 
+    binning_tuples, 
+    optimizer_kwargs=optimizer_kwargs, 
+    max_processes=max_processes,
+    return_evaluations=True,
+)
 
-for i,res in enumerate(collected_results):
-    print(f"------ Bin {i} ------ ")
-    print(res)
+collected_outputs = binned_opt.run()
+
+print()
+
+n_bins = len(collected_outputs)
+for i in range(n_bins):
     print()
+    print(f"Result bin {i}:")
+    print(f"- optimization result:\n {collected_outputs[i][0]}")
+    print(f"- x vals:\n {collected_outputs[i][1]}")
+    print(f"- y vals:\n {collected_outputs[i][2]}")
 
+print()
 
 
